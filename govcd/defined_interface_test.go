f890ff03f029754d92edfb6fdac99506406f8be4
abarreiro <abarreiro@vmware.com>
2023-01-11 12:55:12 +0100

    Improve test
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- govcd/defined_interface_test.go
+++ govcd/defined_interface_test.go
@@ -8,28 +8,33 @@
 package govcd
 
 import (
+	"fmt"
 	"github.com/vmware/go-vcloud-director/v2/types/v56"
 	. "gopkg.in/check.v1"
 	"strings"
 )
 
+// Test_DefinedInterface tests the CRUD behavior of Defined Interfaces. First, it checks how many exist already,
+// as VCD contains some pre-defined ones. Then we create a new one, so the number of existing ones should be increased by 1.
+// We try to get this new created interface with the available getter methods and then perform an update.
+// As a final step, we delete it and check that the deletion is correct (the receiver object is empty and doesn't exist in VCD).
 func (vcd *TestVCD) Test_DefinedInterface(check *C) {
+	if vcd.skipAdminTests {
+		check.Skip(fmt.Sprintf(TestRequiresSysAdminPrivileges, check.TestName()))
+	}
 	endpoint := types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointInterfaces
 	skipOpenApiEndpointTest(vcd, check, endpoint)
-	// TODO: Skip if not admin!
-
-	dummyRde := &types.DefinedInterface{
-		Name:       check.TestName() + "_name",
-		Namespace:  check.TestName() + "_nss",
-		Version:    "1.2.3",
-		Vendor:     "vmware",
-		IsReadOnly: false, // FIXME: Has to be always false???
-	}
 
 	allDefinedInterfaces, err := vcd.client.GetAllDefinedInterfaces(nil)
 	check.Assert(err, IsNil)
 	alreadyPresentRDEs := len(allDefinedInterfaces)
 
+	dummyRde := &types.DefinedInterface{
+		Name:      check.TestName() + "_name",
+		Namespace: check.TestName() + "_nss",
+		Version:   "1.2.3",
+		Vendor:    "vmware",
+	}
 	newDefinedInterface, err := vcd.client.CreateDefinedInterface(dummyRde)
 	check.Assert(err, IsNil)
 	check.Assert(newDefinedInterface, NotNil)
